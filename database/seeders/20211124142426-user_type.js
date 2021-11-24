dbc0eb9b5ae704f36bc6c8ab8ada6f957d6f57c2
DzakiyH <dzakiy.h@mail.ugm.ac.id>
2021-11-24 22:13:19 +0700

    added seeders

--- database/seeders/20211124142426-user_type.js
+++ database/seeders/20211124142426-user_type.js
@@ -1,20 +1,19 @@
 const uuid = require('uuid');
-const bcrypt = require('bcrypt');
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('user_type', [
       {
         id: uuid.v4(),
-        type: customer,
-        createdAt: new Date(),
-        updatedAt: new Date(),
+        type: 'customer',
+        created_at: new Date(),
+        updated_at: new Date(),
       },
       {
         id: uuid.v4(),
-        type: seller,
-        createdAt: new Date(),
-        updatedAt: new Date(),
+        type: 'seller',
+        created_at: new Date(),
+        updated_at: new Date(),
       },
     ]);
   },
---
+++ database/seeders/20211124145252-user.js
@@ -0,0 +1,47 @@
+const uuid = require('uuid');
+const bcrypt = require('bcrypt');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const password = 'pass12345';
+
+    const user_types = await queryInterface.sequelize.query(
+      `SELECT u.id from user_type u`
+    );
+
+    if (user_types && user_types.length > 0) {
+      return Promise.all(
+        user_types.map((type, index) =>
+          queryInterface.bulkInsert('user', [
+            {
+              id: uuid.v4(),
+              email: 'budi@gmail.com',
+              password: bcrypt.hashSync(password, 12),
+              first_name: 'budi',
+              last_name: 'utomo',
+              phone_number: '083345650156',
+              created_at: new Date(),
+              updated_at: new Date(),
+              user_type_id: type[index].id,
+            },
+            {
+              id: uuid.v4(),
+              email: 'susi@gmail.com',
+              password: bcrypt.hashSync(password, 12),
+              first_name: 'susi',
+              last_name: 'utami',
+              phone_number: '088472569821',
+              created_at: new Date(),
+              updated_at: new Date(),
+              user_type_id: type[index].id,
+            },
+          ])
+        )
+      );
+    }
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('user', null);
+  },
+};
