6c15560a0bc893157fecd07544b8b616b93b1560
DzakiyH <dzakiy.h@mail.ugm.ac.id>
2021-12-13 16:56:55 +0700

    database trial 5

--- database/connection.js
+++ database/connection.js
@@ -4,32 +4,30 @@ const { Sequelize } = require('sequelize');
 const { DB_HOST, DB_NAME, DB_PASS, DB_USER, DB_PORT, DB_DIALECT, DB_LOGGING } =
   process.env;
 
-if (process.env.CLEARDB_DATABASE_URL) {
-  const connection = new Sequelize(DB_NAME, {
-    port: parseInt(DB_PORT, 10),
-    dialect: DB_DIALECT,
-    logging: false,
-    pool: {
-      min: 1,
-      max: 5,
-      acquire: 30000,
-      idle: 60000,
-    },
-  });
+const connection = new Sequelize(DB_NAME, {
+  port: parseInt(DB_PORT, 10),
+  dialect: DB_DIALECT,
+  logging: false,
+  pool: {
+    min: 1,
+    max: 5,
+    acquire: 30000,
+    idle: 60000,
+  },
+});
 
-  module.exports = connection;
-} else {
-  const connection = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
-    host: DB_HOST,
-    port: parseInt(DB_PORT, 10),
-    dialect: DB_DIALECT,
-    logging: false,
-    pool: {
-      min: 1,
-      max: 5,
-      acquire: 30000,
-      idle: 60000,
-    },
-  });
-  module.exports = connection;
-}
+module.exports = connection;
+
+// const connection = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
+//   host: DB_HOST,
+//   port: parseInt(DB_PORT, 10),
+//   dialect: DB_DIALECT,
+//   logging: false,
+//   pool: {
+//     min: 1,
+//     max: 5,
+//     acquire: 30000,
+//     idle: 60000,
+//   },
+// });
+// module.exports = connection;
