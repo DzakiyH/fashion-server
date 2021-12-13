109fd6a43e5299f79ba11f3306cbf811aadfbd1a
DzakiyH <dzakiy.h@mail.ugm.ac.id>
2021-12-13 16:46:42 +0700

    fixing database problem

--- database/connection.js
+++ database/connection.js
@@ -4,17 +4,33 @@ const { Sequelize } = require('sequelize');
 const { DB_HOST, DB_NAME, DB_PASS, DB_USER, DB_PORT, DB_DIALECT, DB_LOGGING } =
   process.env;
 
-const connection = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
-  host: DB_HOST,
-  port: parseInt(DB_PORT, 10),
-  dialect: DB_DIALECT,
-  logging: false,
-  pool: {
-    min: 1,
-    max: 5,
-    acquire: 30000,
-    idle: 60000,
-  },
-});
+const connection;
+
+if (process.env.CLEARDB_DATABASE_URL) {
+  connection = new Sequelize(DB_NAME, {
+    port: parseInt(DB_PORT, 10),
+    dialect: DB_DIALECT,
+    logging: false,
+    pool: {
+      min: 1,
+      max: 5,
+      acquire: 30000,
+      idle: 60000,
+    },
+  });
+} else {
+  connection = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
+    host: DB_HOST,
+    port: parseInt(DB_PORT, 10),
+    dialect: DB_DIALECT,
+    logging: false,
+    pool: {
+      min: 1,
+      max: 5,
+      acquire: 30000,
+      idle: 60000,
+    },
+  });
+}
 
 module.exports = connection;
